075aa72a940787fe7d8273d1e246ad4bfaadcdb2
chafidz05 <chafidz0505@gmail.com>
2023-11-13 13:49:11 +0700

    11. Trash Bin

--- app/(main)/_components/item.tsx
+++ app/(main)/_components/item.tsx
@@ -1,8 +1,8 @@
 "use client";
 
 import { 
-  ChevronDown,
-  ChevronRight,
+  ChevronDown, 
+  ChevronRight, 
   LucideIcon,
   MoreHorizontal,
   Plus,
@@ -34,7 +34,7 @@ interface ItemProps {
   level?: number;
   onExpand?: () => void;
   label: string;
-  onClick: () => void;
+  onClick?: () => void;
   icon: LucideIcon;
 };
 
